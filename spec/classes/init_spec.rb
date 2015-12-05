9ab19f796e4f946f102fac81d469331c7de464b0
Trevor Bramwell <tbramwell@linuxfoundation.org>
2015-12-04 18:12:47 -0800

    Define facts in init spec test
    Without any facts defined for the OS, epel, python, and other modules
    fail to compile causing the tests to fail.
    
    Signed-off-by: Trevor Bramwell <tbramwell@linuxfoundation.org>

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,23 @@
 require 'spec_helper'
-describe 'patchwork' do
+describe 'patchwork', :type => 'class' do
 
   context 'with defaults for all parameters' do
+    let (:facts) do { 
+        :osfamily => 'RedHat',
+        :operatingsystem => 'CentOS',
+    }
+    end
+
     it { should contain_class('patchwork') }
+
+    it { should contain_package('git').with_ensure('installed') }
+
+    it do
+      should contain_vcsrepo('/opt/patchwork').with(
+        'ensure'   => 'present',
+        'provider' => 'git',
+        'source'   => 'git://github.com/getpatchwork/patchwork',
+      )
+    end
   end
 end
