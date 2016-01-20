93e228242db18abc873b9f203d098dfa76d14d23
Trevor Bramwell <tbramwell@linuxfoundation.org>
2016-01-20 07:06:04 -0800

    Quote some settings.py values for Python
    Some of the values in settings.py are strings, some are booleans, and
    some are numbers. Only the strings should be quoted.
    
    Signed-off-by: Trevor Bramwell <tbramwell@linuxfoundation.org>

--- spec/classes/config_spec.rb
+++ spec/classes/config_spec.rb
@@ -4,9 +4,12 @@ describe 'patchwork::config', :type => 'class' do
   context 'with defaults for all parameters' do
     let(:params) {{
       :time_zone => 'US/PST',
+      :from_email => '<patchwork@example.com>'
     }}
     it { should compile }
     it { should contain_file('/opt/patchwork/patchwork/settings/production.py')
-           .with_content(/TIME_ZONE = US\/PST/)}
+           .with_content(/TIME_ZONE = 'US\/PST'/)
+           .with_content(/DEFAULT_FROM_EMAIL = '<patchwork@example.com>'/)
+    }
   end
 end
